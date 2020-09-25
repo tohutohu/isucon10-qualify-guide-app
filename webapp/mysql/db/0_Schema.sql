cf24633a29a56e9e14f669116af6523ebe388997
tohutohu <tohu.soy@gmail.com>
2020-09-26 08:53:36 +0900

    rent_id idx

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -33,10 +33,11 @@ CREATE TABLE isuumo.estate
     lat_log POINT AS ((POINT(latitude, longitude))) STORED SRID 0 NOT NULL
     
 );
+CREATE INDEX rentid_idx on isuumo.estate (rent_id);
 CREATE INDEX rent_id_idx on isuumo.estate (rent, id);
-CREATE INDEX rent_id_pupularity_id_idx ON isuumo.estate (rent_id, popularity DESC, id);
-CREATE INDEX door_height_id_pupularity_id_idx ON isuumo.estate (door_height_id, popularity DESC, id);
-CREATE INDEX door_width_pupularity_id_idx ON isuumo.estate (door_width_id, popularity DESC, id);
+CREATE INDEX rentid_pupularity_id_idx ON isuumo.estate (rent_id, popularity DESC, id);
+CREATE INDEX doorheightid_pupularity_id_idx ON isuumo.estate (door_height_id, popularity DESC, id);
+CREATE INDEX doorwidthid_pupularity_id_idx ON isuumo.estate (door_width_id, popularity DESC, id);
 CREATE INDEX lat_log_idx ON isuumo.estate (lat_log);
 
 CREATE TABLE isuumo.chair
