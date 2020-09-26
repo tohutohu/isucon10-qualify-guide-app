2a54ae53304287fe700b4e120ee77d73c33fb73c
tohutohu <tohu.soy@gmail.com>
2020-09-27 01:05:41 +0900

    found_rows

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -540,25 +540,33 @@ func searchChairs(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	searchQuery := "SELECT id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock FROM chair WHERE "
-	countQuery := "SELECT COUNT(*) FROM chair WHERE "
+	searchQuery := "SELECT SQL_CALC_FOUND_ROWS id, name, description, thumbnail, price, height, width, depth, color, features, kind, popularity, stock FROM chair WHERE "
+	countQuery := "SELECT FOUND_ROWS()"
 	searchCondition := strings.Join(conditions, " AND ")
 	limitOffset := " ORDER BY popularity DESC, id ASC LIMIT ? OFFSET ?"
 
 	var res ChairSearchResponse
-	err = chairDb.Get(&res.Count, countQuery+searchCondition, params...)
+
+	tx, err := chairDb.Beginx()
+	defer tx.Commit()
 	if err != nil {
-		c.Logger().Errorf("searchChairs DB execution error : %v", err)
+		c.Logger().Errorf("searchChairs DB get transaction error : %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	chairs := []Chair{}
 	params = append(params, perPage, page*perPage)
-	err = chairDb.Select(&chairs, searchQuery+searchCondition+limitOffset, params...)
+	err = sqlx.Select(tx, &chairs, searchQuery+searchCondition+limitOffset, params...)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, ChairSearchResponse{Count: 0, Chairs: []Chair{}})
 		}
+		c.Logger().Errorf("searchChairs DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	err = sqlx.Get(tx, &res.Count, countQuery)
+	if err != nil {
 		c.Logger().Errorf("searchChairs DB execution error : %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
@@ -741,25 +749,32 @@ func searchEstates(c echo.Context) error {
 		return c.NoContent(http.StatusBadRequest)
 	}
 
-	searchQuery := "SELECT id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity FROM estate WHERE "
-	countQuery := "SELECT COUNT(*) FROM estate WHERE "
+	searchQuery := "SELECT SQL_CALC_FOUND_ROWS id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity FROM estate WHERE "
+	countQuery := "SELECT FOUND_ROWS()"
 	searchCondition := strings.Join(conditions, " AND ")
 	limitOffset := " ORDER BY popularity DESC, id ASC LIMIT ? OFFSET ?"
 
 	var res EstateSearchResponse
-	err = estateDb.Get(&res.Count, countQuery+searchCondition, params...)
+	tx, err := estateDb.Beginx()
+	defer tx.Commit()
 	if err != nil {
-		c.Logger().Errorf("searchEstates DB execution error : %v", err)
+		c.Logger().Errorf("searchEstates DB get transaction error : %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	estates := []Estate{}
 	params = append(params, perPage, page*perPage)
-	err = estateDb.Select(&estates, searchQuery+searchCondition+limitOffset, params...)
+	err = sqlx.Select(tx, &estates, searchQuery+searchCondition+limitOffset, params...)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, EstateSearchResponse{Count: 0, Estates: []Estate{}})
 		}
+		c.Logger().Errorf("searchEstates DB execution error : %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	err = sqlx.Get(tx, &res.Count, countQuery)
+	if err != nil {
 		c.Logger().Errorf("searchEstates DB execution error : %v", err)
 		return c.NoContent(http.StatusInternalServerError)
 	}
