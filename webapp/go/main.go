29a99d4a7cef64116bbdd8b7a56810991e300805
tohutohu <tohu.soy@gmail.com>
2020-09-27 01:31:23 +0900

    set

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -516,7 +516,7 @@ func searchChairs(c echo.Context) error {
 
 	if c.QueryParam("features") != "" {
 		for _, f := range strings.Split(c.QueryParam("features"), ",") {
-			conditions = append(conditions, "features LIKE CONCAT('%', ?, '%')")
+			conditions = append(conditions, "FIND_IN_SET(?, features_set)>0")
 			params = append(params, f)
 		}
 	}
@@ -719,7 +719,7 @@ func searchEstates(c echo.Context) error {
 
 	if c.QueryParam("features") != "" {
 		for _, f := range strings.Split(c.QueryParam("features"), ",") {
-			conditions = append(conditions, "features like concat('%', ?, '%')")
+			conditions = append(conditions, "FIND_IN_SET(?, features_set)>0")
 			params = append(params, f)
 		}
 	}
