3badda53b39b0a6079a6b366b3d4d59eae2d305c
tohutohu <tohu.soy@gmail.com>
2020-09-27 14:26:50 +0900

    フルキャッシュ

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -714,23 +714,38 @@ func postEstate(c echo.Context) error {
 	values := make([]string, 0, 4096)
 	for _, row := range records {
 		rm := RecordMapper{Record: row}
-		id := rm.NextString()
+		id := rm.NextInt()
 		name := rm.NextString()
 		description := rm.NextString()
 		thumbnail := rm.NextString()
 		address := rm.NextString()
-		latitude := rm.NextString()
-		longitude := rm.NextString()
-		rent := rm.NextString()
-		doorHeight := rm.NextString()
-		doorWidth := rm.NextString()
+		latitude := rm.NextFloat()
+		longitude := rm.NextFloat()
+		rent := rm.NextInt()
+		doorHeight := rm.NextInt()
+		doorWidth := rm.NextInt()
 		features := rm.NextString()
-		popularity := rm.NextString()
+		popularity := rm.NextInt()
 		if err := rm.Err(); err != nil {
 			c.Logger().Errorf("failed to read record: %v", err)
 			return c.NoContent(http.StatusBadRequest)
 		}
-		values = append(values, fmt.Sprintf(`(%s, "%s", "%s", "%s", "%s", %s, %s, %s, %s, %s, "%s", %s)`, id, name, description, thumbnail, address, latitude, longitude, rent, doorHeight, doorWidth, features, popularity))
+		values = append(values, fmt.Sprintf(`(%d, "%s", "%s", "%s", "%s", %f, %f, %d, %d, %d, "%s", %d)`, id, name, description, thumbnail, address, latitude, longitude, rent, doorHeight, doorWidth, features, popularity))
+
+		estateMap.Store(strconv.Itoa(id), Estate{
+			ID:          int64(id),
+			Name:        name,
+			Description: description,
+			Thumbnail:   thumbnail,
+			Address:     address,
+			Latitude:    latitude,
+			Longitude:   longitude,
+			Rent:        int64(rent),
+			DoorHeight:  int64(doorHeight),
+			DoorWidth:   int64(doorWidth),
+			Features:    features,
+			Popularity:  int64(popularity),
+		})
 	}
 	_, err = estateDb.Exec("INSERT INTO estate(id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity) VALUES" + strings.Join(values, ","))
 	if err != nil {
